bd3fd38930bbe675373f08c4c296bc5110607df0
vikasAWA <85212839+vikasAWA@users.noreply.github.com>
2024-09-12 13:27:31 +0530

    Method Completed

---
+++ substring.rb
@@ -0,0 +1,15 @@
+def substrings(string, dictionary)
+  dictionary.reduce(Hash.new(0)) do |hash, word_of_dictionary|
+    string.split.each do |word_of_string|
+      if word_of_string.include?(word_of_dictionary)
+        hash[word_of_dictionary] += 1
+      end 
+    end 
+    hash
+  end 
+end 
+
+
+dictionary = ["below","down","go","going","horn","how","howdy","it","i","low","own","part","partner","sit"]
+puts substrings('below', dictionary)
+puts substrings("Howdy partner, sit down! How's it going?", dictionary)
